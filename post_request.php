b48afab18aa56fbe1044eca3ed40db74de40bfd2
HarshPatel2704 <hp210522@gmail.com>
2026-07-03 23:14:35 +0530

    Update post_request.php

--- post_request.php
+++ post_request.php
@@ -56,7 +56,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body class="bg-gray-50">
-    <!-- Navigation -->
     <?php include 'header.php'; ?>
     <!-- Main Content -->
     <div class="max-w-4xl mx-auto px-4 py-12">
